901dfe2052f6a5ca5b66ecd8d79ea4e93cd6e32c
Leandro Peralta <leanndroperalta@gmail.com>
2018-10-08 18:59:11 -0300

    Todas las clases piso, guest, habitacion y hotel

---
+++ src/main/java/com/esolutions/trainings/jsc2/model/Habitacion.java
@@ -0,0 +1,38 @@
+package com.esolutions.trainings.jsc2.model;
+
+import java.util.ArrayList;
+
+public class Habitacion {
+    //Atributos
+    private int numeroHabitacion;
+    ArrayList<Guest> huesped;
+
+    //Constructor
+    public Habitacion(int numeroHabitacion, ArrayList<Guest> p) {
+        this.numeroHabitacion = numeroHabitacion;
+        this.huesped = new ArrayList<>();
+    }
+    //--------------------------------------------------------------------------
+
+    public int getNumeroHabitacion() {
+        return numeroHabitacion;
+    }
+
+    public void setNumeroHabitacion(int numeroHabitacion) {
+        this.numeroHabitacion = numeroHabitacion;
+    }
+
+    public ArrayList<Guest> getHuesped() {
+        return huesped;
+    }
+
+    public void setHuesped(ArrayList<Guest> huesped) {
+        this.huesped = huesped;
+    }
+
+    //metodo para valida Habitacion vacia
+    public boolean estaVaciaH(){
+
+    }
+
+}
