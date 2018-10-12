19213fbd989cc81a350d208342fc08dd15fb321a
Agus <Agus@agus.com>
2018-10-12 20:35:47 -0300

    Agrego la clase PassWifi para separar la logica de la generacion de la contraseña con el controller

--- src/main/java/com/esolutions/trainings/jsc2/model/PassWifi.java
+++ src/main/java/com/esolutions/trainings/jsc2/model/PassWifi.java
@@ -1,4 +1,49 @@
 package com.esolutions.trainings.jsc2.model;
 
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
 public class PassWifi {
+    private ArrayList<String>arreglo = new ArrayList<>();
+
+    private void getCadena(int tamaño)
+    {
+        ArrayList<String>arreglo = new ArrayList<>();
+        arreglo.add("J");
+        boolean primero = true;
+        ArrayList<String> auxiliar = new ArrayList<>();
+
+        String letra;
+        for (int i = 0; i < tamaño; i++) {
+            for (int j = 0; j < arreglo.size(); j++) {
+                letra=arreglo.get(j);
+                switch(letra)
+                {
+                    case "J":
+                        auxiliar.add("J");
+                        auxiliar.add("A");
+                        break;
+                    case "A":
+                        auxiliar.add("V");
+                        auxiliar.add("A");
+                        break;
+                    case "V":
+                        auxiliar.add("V");
+                        auxiliar.add("J");
+                        break;
+
+                }
+
+            }
+            arreglo = auxiliar;
+            auxiliar.clear();
+        }
+    }
+
+    public void imprimir()
+    {
+        for (int i = 0; i < arreglo.size(); i++) {
+            System.out.print(arreglo.get(i));
+        }
+    }
 }
--- src/main/java/com/esolutions/trainings/jsc2/web/PassWIfi.java
+++ src/main/java/com/esolutions/trainings/jsc2/web/PassWIfi.java
@@ -15,47 +15,8 @@ public class PassWIfi {
     @RequestMapping(method = RequestMethod.GET, path = "/floors/{floor}/rooms/{room}/wifi/password")
     public int getPass(@PathVariable int floor, @PathVariable int room){
 
-        int tamaño = floor + room;
-        ArrayList<String>arreglo = new ArrayList<>();
-        arreglo.add("J");
-        boolean primero = true;
-        ArrayList<String> auxiliar = new ArrayList<>();
 
-        Pattern pattern = Pattern.compile("JAVA");
-        String letra;
-        for (int i = 0; i < tamaño; i++) {
-            for (int j = 0; j < arreglo.size(); j++) {
-                letra=arreglo.get(j);
-                switch(letra)
-                {
-                    case "J":
-                        auxiliar.add("J");
-                        auxiliar.add("A");
-                        break;
-                    case "A":
-                        auxiliar.add("V");
-                        auxiliar.add("A");
-                        break;
-                    case "V":
-                        auxiliar.add("V");
-                        auxiliar.add("J");
-                        break;
-
-                }
-
-           }
-           arreglo = auxiliar;
-            auxiliar.clear();
-        }
-
-        Matcher match = pattern.matcher(arreglo.toString());
-        boolean encontrar;
-        int numerodeveces = 0;
-
-        while (encontrar = match.find()){
-            numerodeveces++;
-        }
-        return numerodeveces;
+        return 1;
     }
 
 }
