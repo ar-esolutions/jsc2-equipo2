1e3edcbb34ec8f1b3f1fa36ea0a0f152322619e7
galloafranco@gmail.com <umc2239440909>
2018-10-12 20:08:25 -0300

    Se adapta la respuesta del Wifiresponde segun el requerimiento

--- src/main/java/com/esolutions/trainings/jsc2/web/WifiController.java
+++ src/main/java/com/esolutions/trainings/jsc2/web/WifiController.java
@@ -38,8 +38,11 @@ public class WifiController {
                 }
             }
         }
-
-
-        return new WifiResponse("HAND" + room + floor + aux);
+        if  (aux == 0){
+            return new WifiResponse("HAND" + floor +"-"+ room);
+        } else {
+            return new WifiResponse("HAND" + floor +"-"+ room +"-"+ aux);
+        }
+        //return new WifiResponse("HAND-" + floor +"-"+ room +"-"+ aux);
     }
 }
