367f6ba060616f837785bd156bfe5648a4f95e98
Agus <Agus@agus.com>
2018-10-12 21:07:59 -0300

    Agrego la clase PassWifi para separar la logica de la generacion de la contraseña con el controller

--- src/main/java/com/esolutions/trainings/jsc2/model/TestMain.java
+++ src/main/java/com/esolutions/trainings/jsc2/model/TestMain.java
@@ -2,10 +2,13 @@ package com.esolutions.trainings.jsc2.model;
 
 import com.esolutions.trainings.jsc2.web.PassWIfiController;
 
+import java.util.ArrayList;
+
 public class TestMain {
     public static void main(String[] args) {
 
         PassWifi pw = new PassWifi();
         pw.imprimir();
+
     }
 }
